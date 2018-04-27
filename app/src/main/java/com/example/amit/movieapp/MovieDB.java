aded1f669e10c050531ecb167a9c079be6d0cbcb
Amit Grover <amitg90@gmail.com>
2018-04-27 12:13:34 -0700

    initial checkin

--- app/src/main/java/com/example/amit/movieapp/MovieDB.java
+++
@@ -1,7 +0,0 @@
-package com.example.amit.movieapp;
-
-import java.util.ArrayList;
-
-public class MovieDB {
-    public static ArrayList<MovieInfo> movieInfoArrayList = new ArrayList<MovieInfo>();
-}
--- app/src/main/java/com/example/amit/movieapp/MovieInfo.java
+++
@@ -1,8 +0,0 @@
-package com.example.amit.movieapp;
-
-public class MovieInfo {
-    String path;
-    String title;
-    String release_date;
-    String vote_average;
-}
