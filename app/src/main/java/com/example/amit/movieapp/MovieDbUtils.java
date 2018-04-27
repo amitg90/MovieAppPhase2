e1da23fa7c2fab1d94f7c241c33379754ec36f58
Amit Grover <amitg90@gmail.com>
2018-04-27 15:25:15 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/amit/movieapp/MovieDbUtils.java
@@ -0,0 +1,75 @@
+package com.example.amit.movieapp;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class MovieDbUtils {
+    final static String BASE_URL = "https://api.themoviedb.org/3/movie/";
+    final static String POPULAR_URL = "popular?";
+    final static String HIGHEST_RATED_URL = "top_rated?";
+    final static String IMAGE_BASE_URL = "http://image.tmdb.org/t/p/w185//";
+    final static String API_KEY_URL = "api_key=";
+    final static String API_KEY = "";
+    final static String TAG = "MovieDbUtils";
+    final static String FINAL_POPULAR_URL = BASE_URL + POPULAR_URL + API_KEY_URL + API_KEY;
+    final static String FINAL_HIGHEST_RATED_URL = BASE_URL + HIGHEST_RATED_URL + API_KEY_URL + API_KEY;
+
+    // popular = http://api.themoviedb.org/3/movie/popular?api_key=[YOUR_API_KEY]
+    public static URL buildPopularURL() {
+        Uri builtUri = Uri.parse(FINAL_POPULAR_URL).buildUpon()
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        Log.e(TAG, "Built URI " + url);
+
+        return url;
+    }
+
+    public static URL buildHighestRatedURL() {
+        Uri builtUri = Uri.parse(FINAL_HIGHEST_RATED_URL).buildUpon()
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        Log.e(TAG, "!!!!Built URI " + url);
+
+        return url;
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/amit/movieapp/Settings.java
@@ -0,0 +1,10 @@
+package com.example.amit.movieapp;
+
+enum Selection_Types {
+    Selection_Types_Popularity,
+    Selection_Types_HighestRated
+};
+
+public class Settings {
+    public static Selection_Types selection_types = Selection_Types.Selection_Types_Popularity;
+}
