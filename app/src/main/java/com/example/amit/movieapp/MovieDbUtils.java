095a0a8166ddef65327e11c8d7ac5db38db01e5f
Amit Grover <amitg90@gmail.com>
2018-05-19 18:18:00 -0700

    Addressed some comments from review

--- app/src/main/java/com/example/amit/movieapp/MovieDbUtils.java
+++ app/src/main/java/com/example/amit/movieapp/MovieDbUtils.java
@@ -16,7 +16,7 @@ public class MovieDbUtils {
     final static String HIGHEST_RATED_URL = "top_rated?";
     final static String IMAGE_BASE_URL = "http://image.tmdb.org/t/p/w185//";
     final static String API_KEY_URL = "api_key=";
-    final static String API_KEY = "43b925924f6c2daee9077e7b48ef88c3";
+    final static String API_KEY = "";
     final static String TAG = "MovieDbUtils";
     final static String FINAL_POPULAR_URL = BASE_URL + POPULAR_URL + API_KEY_URL + API_KEY;
     final static String FINAL_HIGHEST_RATED_URL = BASE_URL + HIGHEST_RATED_URL + API_KEY_URL + API_KEY;
